ae14c19811c15926322d19b3aaeae8608ea06ab6
Jim Northrup <jnorthrup@users.noreply.github.com>
2023-07-05 01:50:04 +0800

    Update garia2c.bash
    more nits

--- garia2c.bash
+++ garia2c.bash
@@ -1,81 +1,81 @@
 #!/bin/bash
 
 DISTDIR="$1"
-#if empty show help and exit
+
+# If DISTDIR is empty, show help and exit
 if [ -z "$DISTDIR" ]; then
-cat <<-EOF
-Install make.conf mod to use aria2c for distfiles:
-$0 install
+  cat <<-EOF
+    Install make.conf mod to use aria2c for distfiles:
+    $0 install
 
-Remove the script: 
-$0 remove
+    Remove the script:
+    $0 remove
 
-Normal Usage: $0 DISTDIR FILE URI [MIRRORS...]
-EOF
-exit 1
-fi
-
-#if DISTDIR is "install" then run the make.conf mod
-if [ "$DISTDIR" = "install" ]; then
-
-  cat <<-EOF >>/etc/make.conf
-  FETCHCOMMAND="$0 \\\${DISTDIR} \\\${FILE} \\\${URI} \\\$GENTOO_MIRRORS"
-  RESUMECOMMAND=\$FETCHCOMMAND
+    Normal Usage: $0 DISTDIR FILE URI [MIRRORS...]
 EOF
   exit 1
-fi
-if [ "$DISTDIR" = "remove" ] ; then
-      sed -i -e '/FETCHCOMMAND/d' -e '/RESUMECOMMAND/d' "${ROOT}/etc/make.conf" || die "Failed to remove FETCHCOMMAND and RESUMECOMMAND from make.conf" 
-      exit 1
+elif [ "install" = "$DISTDIR" ]; then
+  cat <<-EOF >>/etc/make.conf
+    FETCHCOMMAND="$0 \\\${DISTDIR} \\\${FILE} \\\${URI} \\\$GENTOO_MIRRORS"
+    RESUMECOMMAND=\$FETCHCOMMAND
+EOF
+  exit 1
+elif [ "remove" = "$DISTDIR" ]; then
+  #if we use sed here to edit the file it will mess up symlinks potentially.  so appending is the way to go
+  grep --perl-regexp '(FETCH|RESUME)COMMAND=' /usr/share/portage/config/make.globals >>/etc/make.conf
+  exit 1
 fi
 
 set -x
 
 FILE="$2"
-# Determine an idempotent temp file
-: tempdir is ${TMPDIR:=/tmp}
 
+# Determine an idempotent temp file
+TMPDIR="${TMPDIR:=/tmp}"
 FILE_HASH=$(echo -n "$FILE" | md5sum | cut -d ' ' -f 1)
 TMPFILE="$TMPDIR/$FILE_HASH"
 
 # If the temp file does not exist, create it
-[[ ! -f "$TMPFILE" ]] && {
+if [[ ! -f "$TMPFILE" ]]; then
   URI="$3"
   shift 3
   # the rest of the arguments are the mirrors
 
-  : the mirrors: $@
-  MIRRORS=($@)
+  echo "the mirrors: $@"
+  MIRRORS=("$@")
 
   pushd "$DISTDIR"
 
   # start building the aria2c command
-  CMD="aria2c -c -j 15 -s 15 -x 15 --remote-time --http-accept-gzip --uri-selector=adaptive  -d $DISTDIR -o $FILE"
+  CMD="aria2c -c -j 15 -s 15 -x 15 --remote-time --http-accept-gzip --uri-selector=adaptive -d $DISTDIR -o $FILE"
   CMD=($CMD)
 
   # find the index of the mirror that is being used as the prefix of the URI
   key_index=0
   for index in "${!MIRRORS[@]}"; do
-    [[ "$URI" == "${MIRRORS[$index]}"* ]] && key_index=$index && break
+    if [[ "${MIRRORS[$index]}"* = "$URI" ]]; then
+      key_index=$index
+      break
+    fi
   done
 
   # for each mirror
   for index in "${!MIRRORS[@]}"; do
     # if the current index is not equal to the key_index
-    [[ $index -ne $key_index ]] && {
+    if [[ $index -ne $key_index ]]; then
       # replace the key mirror in the URI with the current mirror
       MIRROR_URI=$(echo "$URI" | sed "s#${MIRRORS[$key_index]}#${MIRRORS[$index]}#g")
       # add the mirror URI to the command
       CMD+=("$MIRROR_URI")
-    }
+    fi
   done
 
   # Store the aria2c command in the temp file
   echo "${CMD[@]}" >"$TMPFILE"
-}
+fi
 
 # Execute the aria2c command
-. "$TMPFILE" && rm "$TMPFILE" || {
+source "$TMPFILE" && rm "$TMPFILE" || {
   echo "Failed to download $FILE into $DISTDIR from $URI using aria2c in $TMPFILE" with exit code ${errcode:=$?} >&2
   exit $errcode
 }
